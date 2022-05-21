760e39f28585d7deea85e271514bc5f0c9308c91
varela francisco <francisco.varela@etu.umontpllier.fr>
2022-05-21 17:34:07 +0200

    chantillon

--- chantillon.py
+++ chantillon.py
@@ -1,11 +1,3 @@
-def chantillon(fc, n,p):
-    Pt_ech = fc  # #P_ech *10^9 point de echantillon  par 1 segundo
-    t_x = (1 / Pt_ech) * n  # Determination de periode
-    f_x = (1 / t_x) * 1000  # Determination de frequence
-    t_unit = (1 / Pt_ech) * 1  # Temps pour n=1pts
-    t_gating = ((t_x / 2) * p) / 100
-    return (t_x, f_x, t_unit, t_gating)  # #echantillon par ns *temps mise pour total de n
-
 
 def type_sinus(fc, n):
     Pt_ech = fc  # #P_ech *10^9 point de echantillon  par 1 segundo
